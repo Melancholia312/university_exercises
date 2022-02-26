289d2478e6788eb61954054face8f0b04a4a43ee
Melancholia312 <67428510+Melancholia312@users.noreply.github.com>
2022-02-26 12:39:05 +0300

    Add files via upload

--- exercise_6.py
+++ exercise_6.py
@@ -1,16 +1,9 @@
 def find_three_min_nums(arr):
-    res = []
 
     if len(arr) <= 3: #Защита от дурака
         return arr
 
-    for i in range(3): #Находим 3 минимальных числа и добавляем их в результат
-        min_num = min(arr)
-        res.append(min_num)
-        arr.remove(min_num)
-
-        if len(res) == 3:
-            return res
+    return sorted(arr)[:3] #Сортируем и берем первые 3 числа
 
 
 print(find_three_min_nums([2, 1, 6, 1, 0])) # [0, 1, 1]
